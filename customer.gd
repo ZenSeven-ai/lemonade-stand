e2986401d4bb2a946b79e1740b1c17de1c2aa991
Hadijat Oke <hadij@Hadijats-Air.fios-router.home>
2024-06-12 19:51:07 -0400

    fixing navigation so the enimies don't shake when they reach target and mkaing enemies line up at stand

--- customer.gd
+++ customer.gd
@@ -5,10 +5,28 @@ const speed = 60
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
+var stop = false
+
+func _ready():
+	nav_agent.path_desired_distance = 60
+	nav_agent.target_desired_distance = 60
+	
+	
 func _physics_process(delta: float) -> void:
+	
+	
+		
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
-	move_and_slide()
+	nav_agent.set_velocity_forced(velocity)
+	
+	if nav_agent.is_target_reached():
+		velocity = Vector2.ZERO
+		print("done")
+		return
+	
+	
+	
 	
 	
 # finds best way to get to player
@@ -20,3 +38,11 @@ func make_path() -> void:
 # every time the timer times out make a new path to the player
 func _on_timer_timeout():
 	make_path()
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	velocity = velocity.move_toward(safe_velocity, .25)
+	move_and_slide()
+
+
+	
---
+++ scenes/enemy_timer.gd
@@ -0,0 +1,23 @@
+extends Timer
+
+var customer = preload("res://scenes/customer.tscn")
+@onready var thing =  $"../stand/Sprite2D"
+
+func _on_timeout():
+	var e = customer.instantiate()
+	e.position = Vector2(431,290)
+	e.player = thing
+	get_tree().get_root().add_child(e)
+	
+	thing = e
+	
+	await get_tree().create_timer(3.8).timeout
+	
+	e = customer.instantiate()
+	e.position = Vector2(431,290)
+	e.player = thing
+	get_tree().get_root().add_child(e)
+	
+	thing = e
+	
+	await get_tree().create_timer(3.8).timeout
